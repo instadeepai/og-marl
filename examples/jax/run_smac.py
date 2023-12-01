19c82f05451fbbcf904cff01bab9cdabd4730031
Claude Formanek <formanekjc@gmail.com>
2023-12-01 07:14:28 +0200

    Added jax requirements to setup.

--- examples/jax/run_smac.py
+++
@@ -1,7 +0,0 @@
-from og_marl.jax.systems.maicq import train_maicq
-from og_marl.environments.utils import get_environment
-
-eval_env = get_environment("smac_v1", "8m")
-dataset_path = ".experience/Good_8m"
-
-train_maicq(eval_env, dataset_path, batch_size=256, num_epochs=10, num_episodes_per_evaluation=4)
--- setup.py
+++ setup.py
@@ -17,10 +17,10 @@ from setuptools import find_packages, setup
 setup(
     name='OG-MARL',
     version='0.0.1',
-    author='Anon Anonymous',
-    author_email='anon@anonymous.com',
+    author='Claude Formanek',
+    author_email='c.formanek@instadeep.com',
     packages=find_packages(),
-    url='',
+    url='https://sites.google.com/view/og-marl',
     license='',
     description='Off-the-Grid MARL: a Framework for Dataset Generation with Baselines for Cooperative Offline Multi-Agent Reinforcement Learning',
     long_description="",
@@ -35,4 +35,8 @@ setup(
         "absl-py",
         "gymnasium"
     ],
+    extras_require={
+        'jax': ['flashbax', 'optax', "jax", "flax", "orbax-checkpoint"],
+    }
+
 )
