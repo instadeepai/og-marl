218c5dcd9a92db275e160010a7d9bee12b54f033
Callum Tilbury <c.tilbury@instadeep.com>
2024-02-12 15:45:01 +0200

    typing: iddpg_cql

--- og_marl/tf2/systems/iddpg_cql.py
+++ og_marl/tf2/systems/iddpg_cql.py
@@ -13,9 +13,15 @@
 # limitations under the License.
 
 """Implementation of TD3"""
+from typing import Dict
+
 import numpy as np
 import tensorflow as tf
+from chex import Numeric
+from tensorflow import Tensor
 
+from og_marl.environments.base import BaseEnvironment
+from og_marl.loggers import BaseLogger
 from og_marl.tf2.systems.iddpg import IDDPGSystem
 from og_marl.tf2.utils import (
     batch_concat_agent_id_to_obs,
@@ -30,19 +36,19 @@ from og_marl.tf2.utils import (
 class IDDPGCQLSystem(IDDPGSystem):
     def __init__(
         self,
-        environment,
-        logger,
-        linear_layer_dim=64,
-        recurrent_layer_dim=64,
-        discount=0.99,
-        target_update_rate=0.005,
-        critic_learning_rate=1e-3,
-        policy_learning_rate=1e-3,
-        add_agent_id_to_obs=False,
-        random_exploration_timesteps=0,
-        num_ood_actions=10,  # CQL
-        cql_weight=5.0,  # CQL
-        cql_sigma=0.2,  # CQL
+        environment: BaseEnvironment,
+        logger: BaseLogger,
+        linear_layer_dim: int = 64,
+        recurrent_layer_dim: int = 64,
+        discount: float = 0.99,
+        target_update_rate: float = 0.005,
+        critic_learning_rate: float = 1e-3,
+        policy_learning_rate: float = 1e-3,
+        add_agent_id_to_obs: bool = False,
+        random_exploration_timesteps: int = 0,
+        num_ood_actions: int = 10,  # CQL
+        cql_weight: float = 5.0,  # CQL
+        cql_sigma: float = 0.2,  # CQL
     ):
         super().__init__(
             environment=environment,
@@ -62,7 +68,7 @@ class IDDPGCQLSystem(IDDPGSystem):
         self._cql_sigma = cql_sigma
 
     @tf.function(jit_compile=True)  # NOTE: comment this out if using debugger
-    def _tf_train_step(self, batch):
+    def _tf_train_step(self, batch: Dict[str, Tensor]) -> Dict[str, Numeric]:
         batch = batched_agents(self._environment.possible_agents, batch)
 
         # Unpack the batch
