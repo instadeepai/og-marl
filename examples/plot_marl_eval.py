abae12ec86f99912a50cc426d99eecd16b57aa56
Claude Formanek <formanekjc@gmail.com>
2024-05-16 19:16:28 +0200

    exp: goat

--- examples/plot_marl_eval.py
+++ examples/plot_marl_eval.py
@@ -3,21 +3,21 @@
 import json
 
 # Relevant imports
-from marl_eval.plotting_tools.plotting import performance_profiles, sample_efficiency_curves
+from marl_eval.plotting_tools.plotting import performance_profiles, sample_efficiency_curves, aggregate_scores
 from marl_eval.utils.data_processing_utils import create_matrices_for_rliable, data_process_pipeline
 
 # Specify any metrics that should be normalised
 METRICS_TO_NORMALIZE = ["episode_return"]
 
 # Read in and process data
-with open("logs/metrics.json", "r") as f:
+with open("2halfcheetah_chebshev_results.json", "r") as f:
     raw_data = json.load(f)
 
 processed_data = data_process_pipeline(raw_data=raw_data, metrics_to_normalize=METRICS_TO_NORMALIZE)
 
 environment_comparison_matrix, sample_effeciency_matrix = create_matrices_for_rliable(
     data_dictionary=processed_data,
-    environment_name="smac_v1",
+    environment_name="2halfcheetah",
     metrics_to_normalize=METRICS_TO_NORMALIZE,
 )
 
@@ -37,3 +37,12 @@ fig, _, _ = sample_efficiency_curves(
 )
 
 fig.figure.savefig("sample_efficiency.png", bbox_inches="tight")
+
+
+fig, _, _ = aggregate_scores(
+    environment_comparison_matrix,
+    metric_name="episode_return",
+    metrics_to_normalize=METRICS_TO_NORMALIZE,
+)
+
+fig.figure.savefig("aggregate_scores.png", bbox_inches="tight")
