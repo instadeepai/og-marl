603f744a862b76601a9fd7d4c59280d77f5418e0
Claude Formanek <formanekjc@gmail.com>
2024-05-20 21:07:50 +0200

    exp: gota.

--- og_marl/tf2/systems/ff_maddpg.py
+++ og_marl/tf2/systems/ff_maddpg.py
@@ -35,17 +35,18 @@ flags.DEFINE_string("env", "mamujoco", "Environment name.")
 flags.DEFINE_string("scenario", "2ant", "Environment scenario name.")
 flags.DEFINE_string("dataset", "Good", "Dataset type.")
 flags.DEFINE_string("system", "maddpg", "System name.")
-flags.DEFINE_string("joint_action", "buffer", "")
+flags.DEFINE_string("joint_action", "online", "")
 flags.DEFINE_float("trainer_steps", 3e5, "Number of training steps.")
 flags.DEFINE_float("priority_exponent", 0.99, "Priority exponent")
 flags.DEFINE_float("gaussian_steepness", 2, "")
+flags.DEFINE_float("bc_alpha", 2.5, "")
 flags.DEFINE_integer("prioritised_batch_size", 256, "")
 flags.DEFINE_integer("uniform_batch_size", 10024, "")
-flags.DEFINE_integer("update_priorities_every", 1, "")
+flags.DEFINE_integer("update_priorities_every", 0, "")
 flags.DEFINE_integer("seed", 42, "Seed.")
 
-class TransitionBuffer:
 
+class TransitionBuffer:
     def __init__(
         self,
         env_name,
@@ -55,7 +56,7 @@ class TransitionBuffer:
         prioritised_batch_size=256,
         uniform_batch_size=1024,
         priority_exponent=0.99,
-        seed=42
+        seed=42,
     ):
         self._rng_key = jax.random.PRNGKey(seed)
 
@@ -67,7 +68,7 @@ class TransitionBuffer:
         ).read()
 
         # Get example timestep
-        example_timestep = jax.tree_map(lambda x: x[0,0], self._vault_buffer_state.experience)
+        example_timestep = jax.tree_map(lambda x: x[0, 0], self._vault_buffer_state.experience)
 
         # Initialise uniform buffer
         self._uniform_replay_buffer = fbx.make_prioritised_flat_buffer(
@@ -76,8 +77,8 @@ class TransitionBuffer:
             add_batch_size=1,
             add_sequences=True,
             sample_batch_size=uniform_batch_size,
-            priority_exponent=0.0, # No prioritisation
-            device="gpu"
+            priority_exponent=0.0,  # No prioritisation
+            device="gpu",
         )
 
         # Initialise prioritised buffer
@@ -88,7 +89,7 @@ class TransitionBuffer:
             add_sequences=True,
             sample_batch_size=prioritised_batch_size,
             priority_exponent=priority_exponent,
-            device="gpu"
+            device="gpu",
         )
         tmp_buffer_state = self._prioritised_replay_buffer.init(example_timestep)
 
@@ -112,11 +113,12 @@ class TransitionBuffer:
         self._rng_key, sample_key = jax.random.split(self._rng_key, 2)
         batch = self._prioritised_sample_fn(self._buffer_state, sample_key)
         return batch
-    
+
     def update_priorities(self, indices, priorities):
         self._buffer_state = self._set_priorities_fn(
             self._buffer_state, indices, priorities.numpy()
         )
+
 
 class StateAndJointActionCritic(snt.Module):
     def __init__(self, num_agents: int, num_actions: int):
@@ -148,7 +150,6 @@ class StateAndJointActionCritic(snt.Module):
         agent_actions [B,N,A]: the actions the agent took.
         other_actions [B,N,A]: the actions the other agents took.
         """
-
         # Make joint action
         joint_actions = self.make_joint_action(agent_actions, other_actions)
 
@@ -161,7 +162,6 @@ class StateAndJointActionCritic(snt.Module):
         q_values = self._critic_network(critic_input)
 
         return q_values
-
 
     def make_joint_action(self, agent_actions, other_actions):
         """Method to construct the joint action.
@@ -185,21 +185,22 @@ class StateAndJointActionCritic(snt.Module):
 
         return all_joint_actions
 
-class FFMADDPG:
 
+class FFMADDPG:
     def __init__(
-        self, 
-        env, 
-        buffer, 
+        self,
+        env,
+        buffer,
         logger,
-        target_update_rate=0.05,
-        critic_learning_rate=1e-3,
-        policy_learning_rate=1e-3,
+        target_update_rate=0.005,
+        critic_learning_rate=3e-4,
+        policy_learning_rate=3e-4,
+        bc_alpha=2.5,
         update_priorities_every=None,
         joint_action="buffer",
         bc_reg=False,
         cql_reg=False,
-        gaussian_steepness=1
+        gaussian_steepness=1,
     ):
         self.env = env
         self.buffer = buffer
@@ -208,9 +209,9 @@ class FFMADDPG:
         # Policy network
         self.policy_network = snt.Sequential(
             [
-                snt.Linear(64),
+                snt.Linear(256),
                 tf.nn.relu,
-                snt.Linear(64),
+                snt.Linear(256),
                 tf.nn.relu,
                 snt.Linear(self.env._num_actions),
                 tf.nn.tanh,
@@ -232,8 +233,8 @@ class FFMADDPG:
         self.target_update_rate = target_update_rate
 
         # Optimizers
-        self.critic_optimizer = snt.optimizers.RMSProp(learning_rate=critic_learning_rate)
-        self.policy_optimizer = snt.optimizers.RMSProp(learning_rate=policy_learning_rate)
+        self.critic_optimizer = snt.optimizers.Adam(learning_rate=critic_learning_rate)
+        self.policy_optimizer = snt.optimizers.Adam(learning_rate=policy_learning_rate)
 
         # Offline Regularisers
         self.bc_reg = bc_reg
@@ -242,6 +243,7 @@ class FFMADDPG:
         self.discount = 0.99
         self.update_priorities_every = update_priorities_every
         self.gaussian_steepness = gaussian_steepness
+        self.bc_alpha = bc_alpha
 
     @tf.function(jit_compile=True)
     def select_actions(self, observations):
@@ -250,29 +252,29 @@ class FFMADDPG:
         for i, agent in enumerate(agents):
             agent_observation = observations[agent]
 
-            agent_observation = concat_agent_id_to_obs(
-                agent_observation, i, len(agents)
-            )
+            agent_observation = concat_agent_id_to_obs(agent_observation, i, len(agents))
 
             agent_observation = tf.expand_dims(agent_observation, axis=0)  # add batch dimension
-            actions[agent] = self.policy_network(agent_observation)[0] # unbatch
+            actions[agent] = self.policy_network(agent_observation)[0]  # unbatch
 
         return actions
-    
+
     @tf.function(jit_compile=True)
     def compute_new_priorities(self, experience):
         observations = experience.first["observations"]  # (B,N,O)
         actions = experience.first["actions"]  # (B,N,A)
 
-        N,A = actions.shape[-2:]
+        N, A = actions.shape[-2:]
 
         observations = batch_concat_agent_id_to_obs(observations)
 
         target_actions = self.target_policy_network(observations)
 
-        distance = tf.reduce_mean(tf.reduce_mean(tf.abs(actions - target_actions), axis=-1), axis=-1) # L1
+        distance = tf.reduce_mean(
+            tf.reduce_mean(tf.abs(actions - target_actions), axis=-1), axis=-1
+        )  # L1
 
-        priority = tf.exp(-(self.gaussian_steepness * distance)**2)
+        priority = tf.exp(-((self.gaussian_steepness * distance) ** 2))
 
         logs = {
             "Max Priority": tf.reduce_max(priority),
@@ -282,21 +284,21 @@ class FFMADDPG:
             "Mean action distance": tf.reduce_mean(distance),
             "Max action distance": tf.reduce_max(distance),
             "Min action distance": tf.reduce_min(distance),
-            "STD action distance": tf.math.reduce_std(distance)
+            "STD action distance": tf.math.reduce_std(distance),
         }
 
         return logs, priority
 
     @tf.function(jit_compile=True)
-    def train_step(self, experience):
+    def train_step(self, experience, train_step):
         # Unpack the batch
         observations = experience.first["observations"]  # (B,N,O)
-        next_observations = experience.second["observations"] # (B,N,O)
+        next_observations = experience.second["observations"]  # (B,N,O)
         actions = experience.first["actions"]  # (B,N,A)
         env_states = experience.first["infos"]["state"]  # (B,S)
         next_env_states = experience.second["infos"]["state"]  # (B,S)
         rewards = experience.first["rewards"]  # (B,N)
-        terminals = tf.cast(experience.second["terminals"], "float32") # (B,N)
+        terminals = tf.cast(experience.second["terminals"], "float32")  # (B,N)
 
         # Get dims
         B, N, A = actions.shape[:3]
@@ -308,6 +310,9 @@ class FFMADDPG:
 
         # Target policy
         target_actions = self.target_policy_network(next_observations)
+        noise = tf.clip_by_value(tf.random.normal(target_actions.shape, 0, 0.2), -0.5, 0.5)
+        target_actions = target_actions + noise
+        target_actions = tf.clip_by_value(target_actions, -1, 1)
 
         # Target critics
         target_qs_1 = self.target_critic_network_1(next_env_states, target_actions, target_actions)
@@ -322,36 +327,35 @@ class FFMADDPG:
 
         # Do forward passes through the networks and calculate the losses
         with tf.GradientTape(persistent=True) as tape:
-
             ###############
             # Policy Loss #
             ###############
+            if train_step % 2 == 0:
+                # Unroll online policy
+                online_actions = self.policy_network(observations)
 
-            # Unroll online policy
-            online_actions = self.policy_network(observations)
+                if self.joint_action == "buffer":  # Normal MADDPG
+                    other_agent_actions = actions
+                elif self.joint_action == "online":
+                    other_agent_actions = tf.stop_gradient(online_actions)
+                else:
+                    raise ValueError("Not valid joint actions.")
 
-            if self.joint_action == "buffer":  # Normal MADDPG
-                other_agent_actions = actions
-            elif self.joint_action == "online":
-                other_agent_actions = tf.stop_gradient(online_actions)
+                # Evaluate action
+                policy_qs_1 = self.critic_network_1(env_states, online_actions, other_agent_actions)
+                policy_qs_2 = self.critic_network_2(env_states, online_actions, other_agent_actions)
+                policy_qs = tf.minimum(policy_qs_1, policy_qs_2)
+
+                if self.bc_reg:
+                    ##########
+                    # BC Reg #
+                    ##########
+                    bc_lambda = self.bc_alpha / tf.reduce_mean(tf.abs(tf.stop_gradient(policy_qs)))
+                    policy_loss = tf.reduce_mean((actions - online_actions) ** 2) - bc_lambda * tf.reduce_mean(policy_qs)  # + 1e-3 * tf.reduce_mean(online_actions**2)
+                else:
+                    policy_loss = -tf.reduce_mean(policy_qs)
             else:
-                raise ValueError("Not valid joint actions.")
-
-            # Evaluate action
-            policy_qs_1 = self.critic_network_1(env_states, online_actions, other_agent_actions)
-            policy_qs_2 = self.critic_network_2(env_states, online_actions, other_agent_actions)
-            policy_qs = tf.minimum(policy_qs_1, policy_qs_2)
-
-            if self.bc_reg:
-
-                ##########
-                # BC Reg #
-                ##########
-
-                bc_lambda = (2.0 / tf.reduce_mean(tf.abs(tf.stop_gradient(policy_qs))))
-                policy_loss =  tf.reduce_mean((actions - online_actions)**2) - bc_lambda * tf.reduce_mean(policy_qs) # + 1e-3 * tf.reduce_mean(online_actions**2)
-            else:
-                policy_loss = -tf.reduce_mean(policy_qs)
+                policy_loss = 0.0
 
             ###############
             # Critic Loss #
@@ -378,7 +382,6 @@ class FFMADDPG:
             ###########
 
             # TODO
-
 
         # Update critics
         variables = (
@@ -413,18 +416,19 @@ class FFMADDPG:
         logs = {
             "Mean Q-values": tf.reduce_mean((qs_1 + qs_2) / 2),
             "Mean Critic Loss": (critic_loss),
-            "Policy Loss": policy_loss,
+            # "Policy Loss": policy_loss,
         }
 
         return logs
-    
-def concat_agent_id_to_obs(obs, agent_id, N):
 
+
+def concat_agent_id_to_obs(obs, agent_id, N):
     agent_id = tf.one_hot(agent_id, depth=N)
 
     obs = tf.concat([agent_id, obs], axis=-1)
 
     return obs
+
 
 def batch_concat_agent_id_to_obs(obs):
     B, N = obs.shape[:2]  # batch size, num_agents
@@ -442,6 +446,7 @@ def batch_concat_agent_id_to_obs(obs):
 
     return obs
 
+
 def evaluate(env, system, num_eval_episodes: int = 8):
     """Method to evaluate the system online (i.e. in the environment)."""
     episode_returns = []
@@ -453,65 +458,68 @@ def evaluate(env, system, num_eval_episodes: int = 8):
         while not done:
             actions = system.select_actions(observations)
 
-            observations, rewards, terminals, truncations, infos = env.step(
-                actions
-            )
+            observations, rewards, terminals, truncations, infos = env.step(actions)
             episode_return += np.mean(list(rewards.values()), dtype="float")
             done = all(terminals.values()) or all(truncations.values())
         episode_returns.append(episode_return)
     logs = {"evaluator/episode_return": np.mean(episode_returns)}
     return logs
-    
-def train_offline(env, system, buffer, logger, max_trainer_steps=1e6, evaluate_every=5000, num_eval_episodes=8):
-        trainer_step_ctr = 0
-        while trainer_step_ctr < max_trainer_steps:
-            if evaluate_every is not None and trainer_step_ctr % evaluate_every == 0:
-                print("EVALUATION")
-                eval_logs = evaluate(env, system, num_eval_episodes)
-                logger.write(
-                    eval_logs | {"Trainer Steps (eval)": trainer_step_ctr}, force=True
-                )
 
+
+def train_offline(
+    env, system, buffer, logger, max_trainer_steps=1e6, evaluate_every=5000, num_eval_episodes=8
+):
+    trainer_step_ctr = 0
+    while trainer_step_ctr < max_trainer_steps:
+        if evaluate_every is not None and trainer_step_ctr % evaluate_every == 0:
+            print("EVALUATION")
+            eval_logs = evaluate(env, system, num_eval_episodes)
+            logger.write(eval_logs | {"Trainer Steps (eval)": trainer_step_ctr}, force=True)
+
+        start_time = time.time()
+        data_batch = buffer.prioritised_sample()
+        end_time = time.time()
+        time_to_sample = end_time - start_time
+
+        start_time = time.time()
+        train_logs = system.train_step(data_batch.experience, tf.convert_to_tensor(trainer_step_ctr))
+        end_time = time.time()
+        time_train_step = end_time - start_time
+
+        # TODO
+        if (
+            system.update_priorities_every is not None
+            and trainer_step_ctr % system.update_priorities_every == 0
+            and trainer_step_ctr > 10_000
+        ):
             start_time = time.time()
-            data_batch = buffer.prioritised_sample()
+            distance_batch = buffer.uniform_sample()
+            distance_logs, new_priorities = system.compute_new_priorities(distance_batch.experience)
+            buffer.update_priorities(distance_batch.indices, new_priorities)
             end_time = time.time()
-            time_to_sample = end_time - start_time
+            time_priority = end_time - start_time
+            distance_logs["Priority Update Time"] = time_priority
+        else:
+            distance_logs = {}
 
-            start_time = time.time()
-            train_logs = system.train_step(data_batch.experience)
-            end_time = time.time()
-            time_train_step = end_time - start_time
+        train_steps_per_second = 1 / (time_train_step + time_to_sample)
 
-            # TODO
-            if system.update_priorities_every is not None and trainer_step_ctr % system.update_priorities_every == 0 and trainer_step_ctr>10_000:
-                start_time = time.time()
-                distance_batch = buffer.uniform_sample()
-                distance_logs, new_priorities = system.compute_new_priorities(distance_batch.experience)
-                buffer.update_priorities(distance_batch.indices, new_priorities)
-                end_time = time.time()
-                time_priority = end_time - start_time
-                distance_logs["Priority Update Time"] = time_priority
-            else:
-                distance_logs = {}
+        logs = {
+            **train_logs,
+            **distance_logs,
+            "Trainer Steps": trainer_step_ctr,
+            "Time to Sample": time_to_sample,
+            "Time for Train Step": time_train_step,
+            "Train Steps Per Second": train_steps_per_second,
+        }
 
-            train_steps_per_second = 1 / (time_train_step + time_to_sample)
+        logger.write(logs)
 
-            logs = {
-                **train_logs,
-                **distance_logs,
-                "Trainer Steps": trainer_step_ctr,
-                "Time to Sample": time_to_sample,
-                "Time for Train Step": time_train_step,
-                "Train Steps Per Second": train_steps_per_second,
-            }
+        trainer_step_ctr += 1
 
-            logger.write(logs)
-
-            trainer_step_ctr += 1
-
-        print("FINAL EVALUATION")
-        eval_logs = evaluate(env, system, num_eval_episodes)
-        logger.write(eval_logs | {"Trainer Steps (eval)": trainer_step_ctr}, force=True)
+    print("FINAL EVALUATION")
+    eval_logs = evaluate(env, system, num_eval_episodes)
+    logger.write(eval_logs | {"Trainer Steps (eval)": trainer_step_ctr}, force=True)
 
 
 def main(_):
@@ -520,29 +528,31 @@ def main(_):
     download_and_unzip_vault(FLAGS.env, FLAGS.scenario)
 
     buffer = TransitionBuffer(
-        FLAGS.env, 
-        FLAGS.scenario, 
-        FLAGS.dataset, 
-        prioritised_batch_size=FLAGS.prioritised_batch_size, 
+        FLAGS.env,
+        FLAGS.scenario,
+        FLAGS.dataset,
+        prioritised_batch_size=FLAGS.prioritised_batch_size,
         uniform_batch_size=FLAGS.uniform_batch_size,
-        seed=FLAGS.seed
+        seed=FLAGS.seed,
     )
 
-    logger = WandbLogger(
-        entity="off-the-grid-marl-team", project="ff-maddpg"
-    )
+    logger = WandbLogger(entity="off-the-grid-marl-team", project="ff-maddpg")
 
     system_kwargs = {
         "bc_reg": True,
         "joint_action": FLAGS.joint_action,
-        "update_priorities_every":FLAGS.update_priorities_every if FLAGS.update_priorities_every > 0 else None,
-        "gaussian_steepness": FLAGS.gaussian_steepness
-
+        "update_priorities_every": FLAGS.update_priorities_every
+        if FLAGS.update_priorities_every > 0
+        else None,
+        "gaussian_steepness": FLAGS.gaussian_steepness,
+        "bc_alpha": FLAGS.bc_alpha
     }
 
     system = FFMADDPG(env, buffer, logger, **system_kwargs)
 
-    train_offline(env, system, buffer, logger, max_trainer_steps=FLAGS.trainer_steps, evaluate_every=5000)
+    train_offline(
+        env, system, buffer, logger, max_trainer_steps=FLAGS.trainer_steps, evaluate_every=5000
+    )
 
 
 if __name__ == "__main__":
