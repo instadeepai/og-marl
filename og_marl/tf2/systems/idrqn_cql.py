7fe88ce95c8f4280464c876f59afa02064c1158a
Claude Formanek <formanekjc@gmail.com>
2024-06-04 12:00:23 +0200

    exp: cfcql smac.

--- og_marl/tf2/systems/idrqn_cql.py
+++ og_marl/tf2/systems/idrqn_cql.py
@@ -40,7 +40,7 @@ class IDRQNCQLSystem(IDRQNSystem):
         environment: BaseEnvironment,
         logger: BaseLogger,
         num_ood_actions: int = 10,
-        cql_weight: float = 1.0,
+        cql_weight: float = 2.0,
         linear_layer_dim: int = 128,
         recurrent_layer_dim: int = 128,
         discount: float = 0.99,
