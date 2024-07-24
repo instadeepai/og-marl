b7087ad0287691587cc4563f77c6f99959e2feb8
Claude Formanek <formanekjc@gmail.com>
2024-07-24 15:15:59 +0200

    chor: misc clean up.

--- og_marl/tf2/systems/idrqn_bcq.py
+++ og_marl/tf2/systems/idrqn_bcq.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Implementation of QMIX+BCQ"""
+"""Implementation of IDRQN+BCQ"""
 from typing import Any, Dict, Optional
 
 import sonnet as snt
