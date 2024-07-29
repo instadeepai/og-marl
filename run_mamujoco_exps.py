abfee9a4b3225d8d2b04fd6e4115dec4f83f622c
Claude Formanek <formanekjc@gmail.com>
2024-07-29 21:32:48 +0200

    exp: mamujoco

--- run_mamujoco_exps.py
+++ run_mamujoco_exps.py
@@ -14,7 +14,7 @@ EXPERIMENT_CONFIGS = [
     "maddpg_cql/mamujoco/2halfcheetah/Cherry",
 ]  # , "idrqn_cql/smac_v1/5m_vs_6m/Medium_OG_MARL.yaml"]
 WANDB_PROJECT = "lemon-cherry-experiments"
-SEEDS = [5, 6]
+SEEDS = [7, 8]
 
 
 def main(_):
