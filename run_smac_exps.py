e060f48e48edc6de0dd78b0448e124cc4b735929
Claude Formanek <formanekjc@gmail.com>
2024-08-14 12:12:01 +0200

    exp: smac 1

---
+++ run_smac_exps.py
@@ -0,0 +1,29 @@
+import os
+
+from og_marl.tf2.utils import set_growing_gpu_memory
+
+set_growing_gpu_memory()
+
+WANDB_PROJECT = "og-marl-baselines"
+
+SCRIPTS = [
+    "og_marl/tf2/systems/iql_cql.py",
+    "og_marl/tf2/systems/qmix_cql.py",
+    "og_marl/tf2/systems/maicq.py",
+    "og_marl/tf2/systems/iql_bcq.py",
+    "og_marl/tf2/systems/qmix_bcq.py",
+    "og_marl/tf2/systems/discrete_bc.py",
+]
+
+TASK = "task.scenario=5m_vs_6m task.dataset=Good"
+
+SEEDS = [1,2]
+
+def main():
+
+    for seed in SEEDS:
+        for script in SCRIPTS:
+            os.system(f"python {script} {TASK} wandb_project={WANDB_PROJECT}")
+
+if __name__ == "__main__":
+    main()
