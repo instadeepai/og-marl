1ecd685ffbd0e3d015ed598e64b4e52aeb5d4e9a
Claude Formanek <formanekjc@gmail.com>
2024-08-20 16:05:03 +0200

    exp: maddpg+cql sweep medium halfcheetah

--- run_exps.py
+++ run_exps.py
@@ -39,19 +39,19 @@ set_growing_gpu_memory()
 WANDB_PROJECT = "og-marl-mamujoco-baselines"
 
 SCRIPTS = [
-    # "og_marl/tf2/systems/iddpg_cql.py",
+    "og_marl/tf2/systems/iddpg_cql.py",
     # "og_marl/tf2/systems/iddpg_bc.py",
     # "og_marl/tf2/systems/omar.py",
-    # "og_marl/tf2/systems/maddpg_cql.py",
+    "og_marl/tf2/systems/maddpg_cql.py",
     # "og_marl/tf2/systems/continuous_bc.py",
-    # "og_marl/tf2/systems/hacql.py",
+    "og_marl/tf2/systems/hacql.py",
     "og_marl/tf2/systems/maddpg_cql_non_shared.py",
 #    "og_marl/tf2/systems/maddpg_cql_cpg.py"
 ]
 
-TASK = "task.scenario=2halfcheetah task.dataset=Good"
+TASK = "task.scenario=2halfcheetah task.dataset=Medium"
 
-SEEDS = [5] 
+SEEDS = [1] 
 
 def main():
 
