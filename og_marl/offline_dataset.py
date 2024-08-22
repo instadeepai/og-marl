132bd262dcea13773b989d5a3eea68ae7613b172
Claude Formanek <formanekjc@gmail.com>
2024-08-22 13:28:28 +0200

    exp: std matters.

--- og_marl/offline_dataset.py
+++ og_marl/offline_dataset.py
@@ -84,7 +84,7 @@ VAULT_INFO = {
     },
     "mpe_omar": {
         "simple_spread": {
-            "url": "https://huggingface.co/datasets/InstaDeepAI/og-marl/resolve/main/prior_work/omar/mpe/simple_spread_mean.zip"
+            "url": "https://huggingface.co/datasets/InstaDeepAI/og-marl/resolve/main/prior_work/omar/mpe/simple_spread_std.zip"
         },
         
     },
