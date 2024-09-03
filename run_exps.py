8d72abe7fb94f1d144fd266364c80a7f0f541ed2
Claude Formanek <formanekjc@gmail.com>
2024-09-03 17:34:34 +0200

    exp: haddpg+cql

--- run_exps.py
+++ run_exps.py
@@ -42,9 +42,9 @@ SCRIPTS = [
     # "og_marl/tf2/systems/iddpg_cql.py",
     # "og_marl/tf2/systems/iddpg_bc.py",
     # "og_marl/tf2/systems/omar.py",
-    # "og_marl/tf2/systems/maddpg_cql.py",
+    "og_marl/tf2/systems/maddpg_cql.py",
     # "og_marl/tf2/systems/continuous_bc.py",
-    "og_marl/tf2/systems/haddpg_cql.py",
+    # "og_marl/tf2/systems/haddpg_cql.py",
     # "og_marl/tf2/systems/maddpg_cql_non_shared.py",
 #    "og_marl/tf2/systems/maddpg_cql_cpg.py"
 ]
