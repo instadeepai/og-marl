5ab48c33d773d03eeb9dac1250680f9a5017b133
Claude Formanek <formanekjc@gmail.com>
2024-09-08 12:13:47 +0200

    exp: hopper and ant.

--- run_exps.py
+++ run_exps.py
@@ -40,9 +40,9 @@ WANDB_PROJECT = "haddpg+cql-vs-maddpg+cql-2walker"
 SCRIPTS = [
     # "og_marl/tf2/systems/iddpg_cql.py",
     # "og_marl/tf2/systems/iddpg_bc.py",
+    "og_marl/tf2/offline/maddpg_cql.py",
     "og_marl/tf2/offline/omar.py",
     "og_marl/tf2/offline/haddpg_cql.py",
-    "og_marl/tf2/offline/maddpg_cql.py",
     # "og_marl/tf2/systems/continuous_bc.py",
     # "og_marl/tf2/systems/haddpg_cql.py",
     # "og_marl/tf2/systems/maddpg_cql_non_shared.py",
@@ -57,7 +57,7 @@ TASKS = [
 training_steps = int(5e5)
 
 
-SEEDS = [6]
+SEEDS = [5]
 
 
 def main():
