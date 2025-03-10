e0f1c30a9aa6d6a6b8ac809c51c9e0775a73edf3
Claude Formanek <formanekjc@gmail.com>
2025-03-10 12:44:39 +0200

    exp: distributional 5m_vs_6m

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-python og_marl/tf2_systems/online/iql.py seed=2 task.scenario=5m_vs_6m
-python og_marl/tf2_systems/online/ic51.py seed=2 task.scenario=5m_vs_6m
-python og_marl/tf2_systems/online/iqrdqn.py seed=2 task.scenario=5m_vs_6m
+python og_marl/tf2_systems/online/iqrdqn.py seed=3 task.scenario=5m_vs_6m
+python og_marl/tf2_systems/online/iql.py seed=3 task.scenario=5m_vs_6m
+python og_marl/tf2_systems/online/ic51.py seed=3 task.scenario=5m_vs_6m
+
