e2f2dbc6235f7705af188111bb085580e17eb9b3
Claude Formanek <formanekjc@gmail.com>
2025-04-04 12:16:19 +0200

    exp: rware small-2ag Expert

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python og_marl/tf2_systems/offline/maicq.py -m seed=3,4 task.source=alberdice task.env=rware task.scenario=small-2ag task.dataset=Expert
+python og_marl/tf2_systems/offline/maicq.py -m seed=5,6 task.source=alberdice task.env=rware task.scenario=small-2ag task.dataset=Expert
