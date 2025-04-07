f590e3d2c6071300d1a3acba279141090d4ca7f7
Claude Formanek <formanekjc@gmail.com>
2025-04-07 15:18:17 +0200

    exp: rware tiny-2ag Expert

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python og_marl/tf2_systems/offline/maicq.py -m seed=9 task.source=alberdice task.env=rware task.scenario=tiny-2ag task.dataset=Expert
+python og_marl/tf2_systems/offline/maicq.py -m seed=10 task.source=alberdice task.env=rware task.scenario=tiny-2ag task.dataset=Expert
